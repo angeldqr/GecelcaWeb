158d1cc0d2bbb772dfc5980ce861b139aa8d9daa
Angel <angelquinteror102@gmail.com>
2025-01-24 11:00:26 -0500

    Pruebas para mejorar el diseño del Dashboard

--- static/js/Dashboard.js
+++ static/js/Dashboard.js
@@ -1,21 +1,28 @@
-const toggle = document.querySelector(".toggle")
-const menuDashboard = document.querySelector(".menu-dashboard")
-const iconoMenu = toggle.querySelector("i")
-const enlacesMenu = document.querySelectorAll(".enlace")
+const toggle = document.querySelector(".toggle");
+const menuDashboard = document.querySelector(".menu-dashboard");
+const iconoMenu = toggle.querySelector("i");
+const toggleDark = document.getElementById("toggle-dark");
+const iconoDark = toggleDark.querySelector("i");
+const textoDark = toggleDark.querySelector("span");
 
+// Expandir/colapsar menú
 toggle.addEventListener("click", () => {
-    menuDashboard.classList.toggle("open")
+    menuDashboard.classList.toggle("open");
+    iconoMenu.classList.toggle("bx-menu");
+    iconoMenu.classList.toggle("bx-x");
+});
 
-    if(iconoMenu.classList.contains("bx-menu")){
-        iconoMenu.classList.replace("bx-menu", "bx-x")
-    }else {
-        iconoMenu.classList.replace("bx-x", "bx-menu")
+// Cambiar entre Modo Oscuro y Modo Claro
+toggleDark.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        // Cambiar a "Modo claro"
+        textoDark.textContent = "Modo claro";
+        iconoDark.classList.replace("bx-moon", "bx-sun");
+    } else {
+        // Cambiar a "Modo oscuro"
+        textoDark.textContent = "Modo oscuro";
+        iconoDark.classList.replace("bx-sun", "bx-moon");
     }
-})
-
-enlacesMenu.forEach(enlace => {
-    enlace.addEventListener("click", () => {
-        menuDashboard.classList.add("open")
-        iconoMenu.classList.replace("bx-menu", "bx-x")
-    })
-})
+});
