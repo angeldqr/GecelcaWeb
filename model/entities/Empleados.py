1ea0562e3c27891d75c8328e689191f6d4a94176
Angel <angelquinteror102@gmail.com>
2025-01-24 10:59:50 -0500

    Adición del campo "correo_electronico".

--- model/entities/Empleados.py
+++ model/entities/Empleados.py
@@ -7,6 +7,7 @@ class Empleado(db.Model):
     segundo_nombre = db.Column(db.String(50))
     primer_apellido = db.Column(db.String(50), nullable=False)
     segundo_apellido = db.Column(db.String(50))
+    correo_electronico = db.Column(db.String(150), unique=True, nullable=False)
     id_cargo = db.Column(db.Integer, db.ForeignKey('cargos.id_cargo'), nullable=False)
     id_sede = db.Column(db.Integer, db.ForeignKey('sedes.id_sede'), nullable=False)
     id_empresa = db.Column(db.Integer, db.ForeignKey('empresas.id_empresa'), nullable=False)
@@ -16,11 +17,13 @@ class Empleado(db.Model):
     estado = db.Column(db.Boolean, default=True, nullable=False)
 
     def __init__(self, primer_nombre, segundo_nombre, primer_apellido, segundo_apellido,
-                 id_cargo, id_sede, id_empresa, id_area, fecha_nacimiento, fecha_ingreso, estado=True):
+                 correo_electronico, id_cargo, id_sede, id_empresa, id_area,
+                 fecha_nacimiento, fecha_ingreso, estado=True):
         self.primer_nombre = primer_nombre
         self.segundo_nombre = segundo_nombre
         self.primer_apellido = primer_apellido
         self.segundo_apellido = segundo_apellido
+        self.correo_electronico = correo_electronico
         self.id_cargo = id_cargo
         self.id_sede = id_sede
         self.id_empresa = id_empresa
@@ -36,6 +39,7 @@ class Empleado(db.Model):
             "segundo_nombre": self.segundo_nombre,
             "primer_apellido": self.primer_apellido,
             "segundo_apellido": self.segundo_apellido,
+            "correo_electronico": self.correo_electronico,
             "id_cargo": self.id_cargo,
             "id_sede": self.id_sede,
             "id_empresa": self.id_empresa,
